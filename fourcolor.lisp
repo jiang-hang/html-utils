0e2bc1ff9d32d6fe34da52f8b9b9016b9137310a
jiang-hang <jianghang@bagualu.net>
2016-09-02 16:24:33 +0800

    update

--- fourcolor.lisp
+++ fourcolor.lisp
@@ -1,12 +1,13 @@
+#! /usr/bin/sbcl --script
+
+(load "~/quicklisp/setup.lisp")
+
 (ql:quickload "closure-html")
 (ql:quickload "cxml-stp")
 (ql:quickload "cl-ppcre")
+(ql:quickload "external-program")
 
 (load "./html-utils.lisp")
-
-(defvar *doc*)
-
-(setf *doc* (build-doc-from-path "/home/xuyang/html-utils/fourcolor.html"))
 
 (defmacro style-attribute-value (name style)
   `(multiple-value-bind (x attr) (cl-ppcre:scan-to-strings (concatenate 'string ,name ":([0-9]+)px") style)
@@ -15,14 +16,60 @@
 	 (parse-integer (elt attr 0) :junk-allowed t)
 	 (equal 1 0))))
 
-(delete-recursively *doc*
-  (and (typep ch 'stp:element)
-       (or (equal (stp:local-name ch) "div")
-	   (equal (stp:local-name ch) "span"))
-       (let* ((style (stp:attribute-value ch "style"))
-	      (top (style-attribute-value "top" style)))
-	 (if top (< top 241) (equal 1 0)))))
+(defvar *input-htmls* (rest sb-ext:*posix-argv*))
+
+(defvar *doc*)
+
+(dotimes (i 10)
+  (external-program:run "pdf2txt.py" `("-Y" "loose" "-t" "html" "-p"
+					    ,(format nil "~d" i)
+					    "-o"
+					    ,(format nil "p_~d.html" i)
+					    ,(first *input-htmls*)))
 
 
-(format t "~A~%" (cxml2str *doc*))
+  (setf *doc* (build-doc-from-path (format nil "p_~d.html" i)))
+
+
+  (delete-recursively *doc*
+    (and (typep ch 'stp:element)
+	 (or (equal (stp:local-name ch) "div")
+	     (equal (stp:local-name ch) "span"))
+	 (let* ((style (stp:attribute-value ch "style"))
+		(top (style-attribute-value "top" style))
+		(height (style-attribute-value "height" style))
+		)
+	   (if (and top height) ; both top and height are defined
+	       (and (< top 241) (< height 300))
+	       (if top
+		   (or (equal top 50) (equal top 0)) ; remove the top and bottom page no
+		   (equal 1 0))))))
+
+
+					;trans font-size 17px to header 2
+					;
+  (stp:do-recursively (n *doc*)
+    (when  (and (typep n 'stp:element)
+		(equal (stp:local-name n) "span"))
+      (let ((style (stp:attribute-value n "style")))
+	(when (and style
+		   (equal (style-attribute-value "font-size" style) 17))
+	  (setf (stp:local-name n) "h2")))))
+
+
+
+  (with-open-file (stream (format nil "./p_~dout.html" i) :direction :output :if-exists :supersede)
+    (format stream "~A~%" (cxml2str *doc*)))
+
+					;convert it to md
+  (external-program:run "pandoc" `("-f" "html" "-t" "markdown" "-o"
+					,(format nil "./p_~d.md" i)
+					,(format nil "./p_~dout.html" i))) 
+  (format t "ready to use the filter")
+  (external-program:run "pandoc" `("-f" "markdown"
+					"-t" "markdown"
+					"--filter" "/home/xuyang/meteorTrans/stripspan.py"
+					"-o" ,(format nil "./filtered_p~d.md" i)
+					,(format nil "./p_~d.md" i))) 
+  )
 
